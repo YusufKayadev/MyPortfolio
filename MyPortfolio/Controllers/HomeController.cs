d6d58f17f10be01614d3aa7f50cc385e6d52dc8e
yusuf <yusuf@047>
2025-11-03 17:01:03 +0300

    Güncel sürüm ve yeni özellikler eklendi

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace MyPortfolio.Controllers
         {
             return View();
         }
-
+        
         public IActionResult Privacy()
         {
             return View();
---
+++ MyPortfolio/DAL/Entities/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class Message
+    {
+        public int MessageId {  get; set; }
+        public string NameSurname{  get; set; }
+        public string Subject{  get; set; }
+        public string Email{  get; set; }
+        public string MessageDetail1{  get; set; }
+        public DateTime SendDate { get; set; }
+        public bool İsReady {  get; set; }
+    }
+}
